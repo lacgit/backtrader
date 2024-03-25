d9baef969e4c1904c1020291c9e8d94303e607e8
pd <pd@comerich.com>
2024-03-25 08:51:06 +0800

    Catch up the changes from Mememtum's version, the fork did not bring along this change even though it was the fork of the master branch.

--- backtrader/plot/locator.py
+++ backtrader/plot/locator.py
@@ -223,10 +223,19 @@ class AutoDateLocator(ADLocator):
 
         locator.set_axis(self.axis)
 
-    #   locator.set_view_interval(*self.axis.get_view_interval())
-    #   locator.set_data_interval(*self.axis.get_data_interval())
+        try:
+            # try for matplotlib < 3.6.0
+            locator.set_view_interval(*self.axis.get_view_interval())
+            locator.set_data_interval(*self.axis.get_data_interval())
+        except Exception as e:
+            try:
+                # try for matplotlib >= 3.6.0
+                self.axis.set_view_interval(*self.axis.get_view_interval())
+                self.axis.set_data_interval(*self.axis.get_data_interval())
+                locator.set_axis(self.axis)
+            except Exception as e:
+                print("Error:", e)
 
-        self.axis.set_view_interval(*self.axis.get_view_interval())
         return locator
 
 
